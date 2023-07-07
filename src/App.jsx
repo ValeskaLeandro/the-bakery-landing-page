46b6f7b2f41c6f75c40ff409069d9048c7fbd84a
Valeska Leandro <valeskaleandro@gmail.com>
2023-07-07 15:45:40 -0300

    ✨ feat: Section contact + footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+/* eslint-disable react/jsx-no-target-blank */
 import './App.css'
+
 import Contact from './components/Contact/Contact'
 import Header from './components/Header/Header'
 import MainBanner from './components/MainBanner/MainBanner'
@@ -6,6 +8,7 @@ import OurServices from './components/OurServices/Ourservices'
 import Products from './components/Products/Produts'
 import Team from './components/Team/Team'
 
+import { FaLinkedinIn, FaGithub, FaInstagram} from "react-icons/fa";
 function App() {
   return (
     <>
@@ -15,6 +18,14 @@ function App() {
     <Products />
     <Team />
     <Contact />
+    <footer>
+      <span>Valeska Leandro &copy; Todos os direitos reservados</span>
+      <div className='social'>
+        <span><a href="https://www.linkedin.com/in/valeskaleandro/" target='_blank'><FaLinkedinIn color='#c6713a' size={20}/></a></span>
+        <span><a href="https://github.com/ValeskaLeandro" target='_blank'><FaGithub color='#c6713a' size={20}/></a> </span>
+        <span><a href="https://www.instagram.com/valeskaleandro/" target='_blank'><FaInstagram color='#c6713a' size={20}/></a> </span>
+      </div> 
+    </footer>
     </>
   )
 }
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,6 +2,20 @@ import styles from "./Contact.module.css"
 
 export default function Contact() {
   return(
-    <div className={styles.container} id="contact"></div>
+    <div className={styles.container} id="contact">
+      <div className={styles.contact}>
+        <div className={styles.left_contact}>
+          <h3>Entre em contato</h3>
+          <p>Entre em contato para fazer seu orçamento ou pedido.</p>
+          <form className={styles.form}>
+            <input type="text" placeholder="Digite seu nome..." />
+            <input type="text" placeholder="Digite seu e-mail..." />
+            <textarea name="" id="" cols="30" rows="5" placeholder="Digite sua mensagem..."></textarea>
+            <button>Enviar</button>
+          </form>
+        </div>      
+        <img src="/production.jpg" alt="The Bakery" />        
+      </div>
+    </div>
   )
 }
